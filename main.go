a090470abe954dd7b1380730645777d634f1df57
Muhammad Rivaldy Was Here! <muhammadrivaldy16@gmail.com>
2025-03-02 23:11:05 +0700

    Fix failure algorithms for selection and insertion

--- main.go
+++ main.go
@@ -102,7 +102,7 @@ func bubbleSort(arr []int) []int {
 
 func selectionSort(arr []int) []int {
 
-	for _, i := range arr {
+	for i := 0; i < len(arr); i++ {
 
 		minIndex := i
 
@@ -123,15 +123,15 @@ func selectionSort(arr []int) []int {
 
 func insertionSort(arr []int) []int {
 
-	for _, i := range arr {
+	for i := 0; i < len(arr); i++ {
 
 		insertIndex := i
 		currentValue := arr[i]
 
-		for _, j := range arr[:i] {
+		for j := (i - 1); j >= 0; j-- {
 
 			if arr[j] > currentValue {
-				arr[j+1] = arr[j]
+				arr[(j + 1)] = arr[j]
 				insertIndex = j
 			} else {
 				break
