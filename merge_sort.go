13ff2b722f25743fc0b671157fb225dbe2cb2ddf
Muhammad Rivaldy Was Here! <muhammadrivaldy16@gmail.com>
2025-03-06 21:01:15 +0700

    Remove merge sort, I will try to build it from zero

--- merge_sort.go
+++ merge_sort.go
@@ -1,38 +1,5 @@
 package main
 
 func mergeSort(arr []int) []int {
-
-	if len(arr) == 1 {
-		return arr
-	}
-
-	midArr := len(arr) / 2
-
-	leftArr := mergeSort(arr[:midArr])
-	rightArr := mergeSort(arr[midArr:])
-
-	leftIdx := 0
-	rightIdx := 0
-	newArr := []int{}
-
-	for {
-
-		if len(leftArr) > leftIdx && len(rightArr) > rightIdx {
-
-			if leftArr[leftIdx] > rightArr[rightIdx] {
-				newArr = append(newArr, rightArr[rightIdx])
-				rightIdx++
-			} else {
-				newArr = append(newArr, leftArr[leftIdx])
-				leftIdx++
-			}
-		} else {
-			break
-		}
-	}
-
-	newArr = append(newArr, leftArr[leftIdx:]...)
-	newArr = append(newArr, rightArr[rightIdx:]...)
-
-	return newArr
+	return arr
 }
