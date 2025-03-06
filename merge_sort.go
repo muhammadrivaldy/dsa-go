6f8944ec4ccfae6767e0d54a1feeec886eed3b98
Muhammad Rivaldy Was Here! <muhammadrivaldy16@gmail.com>
2025-03-06 21:11:20 +0700

    Finished reset merge sort problem

--- merge_sort.go
+++ merge_sort.go
@@ -1,5 +1,32 @@
 package main
 
 func mergeSort(arr []int) []int {
-	return arr
+
+	if len(arr) == 1 {
+		return arr
+	}
+
+	midArr := len(arr) / 2
+
+	leftArr := mergeSort(arr[:midArr])
+	rightArr := mergeSort(arr[midArr:])
+
+	leftIdx := 0
+	rightIdx := 0
+	newArr := []int{}
+
+	for leftIdx < len(leftArr) && rightIdx < len(rightArr) {
+		if leftArr[leftIdx] < rightArr[rightIdx] {
+			newArr = append(newArr, leftArr[leftIdx])
+			leftIdx++
+		} else {
+			newArr = append(newArr, rightArr[rightIdx])
+			rightIdx++
+		}
+	}
+
+	newArr = append(newArr, leftArr[leftIdx:]...)
+	newArr = append(newArr, rightArr[rightIdx:]...)
+
+	return newArr
 }
