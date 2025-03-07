080efd163a9d41241aefeaeec4cfc1c2dc96d753
Muhammad Rivaldy Was Here! <muhammadrivaldy16@gmail.com>
2025-03-07 09:42:29 +0700

    Remove quick sort, I will try to build it from zero

--- quick_sort.go
+++ quick_sort.go
@@ -2,29 +2,5 @@ package main
 
 func quickSort(arr []int, low, high int) []int {
 
-	if low < high {
-
-		pivot := quickSortPartition(arr, low, high)
-		quickSort(arr, low, pivot-1)
-		quickSort(arr, pivot+1, high)
-	}
-
 	return arr
-}
-
-func quickSortPartition(arr []int, low, high int) int {
-
-	i := low
-
-	for j := low; j < high; j++ {
-
-		if arr[j] <= arr[high] {
-			arr[i], arr[j] = arr[j], arr[i]
-			i += 1
-		}
-	}
-
-	arr[i], arr[high] = arr[high], arr[i]
-
-	return i
 }
