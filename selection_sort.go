f7eb2c7ba6fd2724d6753f6f50b780d66c2cc377
Muhammad Rivaldy Was Here! <muhammadrivaldy16@gmail.com>
2025-03-07 08:42:43 +0700

    Finished reset selection sort problem

--- selection_sort.go
+++ selection_sort.go
@@ -2,5 +2,20 @@ package main
 
 func selectionSort(arr []int) []int {
 
+	for i := 0; i < len(arr); i++ {
+
+		minIdx := i
+
+		for j := (i + 1); j < len(arr); j++ {
+			if arr[j] < arr[minIdx] {
+				minIdx = j
+			}
+		}
+
+		if i != minIdx {
+			arr[i], arr[minIdx] = arr[minIdx], arr[i]
+		}
+	}
+
 	return arr
 }
