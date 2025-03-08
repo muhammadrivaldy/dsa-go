bbe45e0c90c4650e61a5bc36c9858c029ede5d91
Muhammad Rivaldy Was Here! <muhammadrivaldy16@gmail.com>
2025-03-08 23:37:23 +0700

    Remove insertion sort, I will try to build it from zero

--- insertion_sort.go
+++ insertion_sort.go
@@ -2,23 +2,5 @@ package main
 
 func insertionSort(arr []int) []int {
 
-	for i := 0; i < len(arr); i++ {
-
-		insertIdx := i
-		copyValue := arr[i]
-
-		for j := (i - 1); j >= 0; j-- {
-
-			if arr[j] > copyValue {
-				arr[(j + 1)] = arr[j]
-				insertIdx = j
-			} else {
-				break
-			}
-		}
-
-		arr[insertIdx] = copyValue
-	}
-
 	return arr
 }
