404459d4c5cee700bafcc30a34634af8aafad289
Muhammad Rivaldy Was Here! <muhammadrivaldy16@gmail.com>
2025-03-09 14:41:06 +0700

    Finished reset insertion sort problem

--- insertion_sort.go
+++ insertion_sort.go
@@ -2,5 +2,24 @@ package main
 
 func insertionSort(arr []int) []int {
 
+	// input [3, 2, 4, 1]
+
+	for i := 0; i < len(arr); i++ {
+
+		insertIdx := i
+		currentValue := arr[i]
+
+		for j := (i - 1); j >= 0; j-- {
+			if arr[j] > currentValue {
+				arr[j+1] = arr[j]
+				insertIdx = j
+			} else {
+				break
+			}
+		}
+
+		arr[insertIdx] = currentValue
+	}
+
 	return arr
 }
