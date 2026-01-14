bd40838c53f7cbc3c2c82813be5b6db816da5c71
Rivaldy SBS Was Here! Watch Out! <muhammadrivaldy16@gmail.com>
2026-01-14 23:32:11 +0700

    Re-learning sorting 5

--- count_sort.go
+++ count_sort.go
@@ -4,7 +4,6 @@ func countSort(arr []int) []int {
 
 	max := getMax(arr)
 	countArr := make([][]int, (max + 1))
-
 	for _, i := range arr {
 		countArr[i] = append(countArr[i], i)
 	}
--- selection_sort.go
+++ selection_sort.go
@@ -3,12 +3,15 @@ package main
 func selectionSort(arr []int) []int {
 
 	for i := 0; i < len(arr); i++ {
+
 		selectIdx := i
+
 		for j := (i + 1); j < len(arr); j++ {
-			if arr[selectIdx] > arr[j] {
+			if arr[j] < arr[selectIdx] {
 				selectIdx = j
 			}
 		}
+
 		arr[selectIdx], arr[i] = arr[i], arr[selectIdx]
 	}
 
